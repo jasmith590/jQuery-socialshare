4f31fbf0567cf8659882c77775fedc85d872148a
Jamie Smith <jasmith590@gmail.com>
2015-03-15 17:45:44 -0400

    fixed some more shit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="font-awesome.css">
     <link rel="stylesheet" href="style.css">
     <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script src="jQuery.socialshare.js"></script>
+    <script src="jquery.socialshare.js"></script>
     <script type="text/javascript">
         jQuery(document).ready(function($){
             $('.share').socialSharer({debug: false});
